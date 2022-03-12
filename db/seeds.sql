f1ab96b4353a93cfc47da2a62089e6fe3dd33dbe
Ben <bgrowcott@hotmail.co.uk>
2022-03-12 13:38:13 +0000

    added seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO movies (movie_name)
+VALUES ("Spiderman"),
+       ("Batman"),
+       ("Avengers");
+
+INSERT INTO reviews (review, movie_id)
+VALUES ("good", 1),
+       ("bad", 2),
+       ("great", 3);
